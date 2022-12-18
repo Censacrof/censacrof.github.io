0a6328964ab94775cfc99605210dae240d93b518
Francesco Galisi <galisifrancesco@gmail.com>
2022-12-18 23:52:31 +0100

    Added ScrollButton

---
+++ components/ScrollButton.tsx
@@ -0,0 +1,12 @@
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const ScrollButton = () => {
+  return (
+    <div className="fixed mx-auto inset-x-0 flex flex-row bottom-8 text-surface text-5xl justify-center items-center w-20 h-20">
+      <FontAwesomeIcon icon={ faAngleDown } />
+    </div>
+  )
+}
+
+export default ScrollButton
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import Header from '../components/Header'
 import Prompt from '../components/Prompt'
+import ScrollButton from '../components/ScrollButton'
 
 export default function Home() {
   return (
     <div className="container mx-auto px-5 py-4">
-      <div className='flex flex-col'>
+      <div className='flex flex-col items-center'>
         <Header />
         <Prompt text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil' />
       </div>
+      <ScrollButton />
     </div>
   )
 }
