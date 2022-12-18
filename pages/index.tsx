8c6e1d48a2ee6c50d65e6d61ee3ec505c092a33f
Francesco Galisi <galisifrancesco@gmail.com>
2022-12-18 22:06:14 +0100

    Added effect to make cursor blink

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Monda } from '@next/font/google'
+import { useEffect, useState } from 'react'
 
 const monda = Monda({weight: '400'})
 
@@ -8,19 +9,44 @@ const Header = () => {
       <div className='flex-grow flex flex-row justify-center items-center'>
         <div className="bg-primary rounded-full h-[70px] w-[70px]"></div>
       </div>
-      <div className="flex flex-col justify-center gap-2 items-center">
-        <h1 className='text-3xl text-surface'>Francesco Galisi</h1>
-        <p className='text-3xl font-barcode-text text-surface'>Software Engineer</p>
+      <div className="flex flex-col justify-center gap-2 items-center text-surface text-3xl ">
+        <h1 className='text-3xl'>Francesco Galisi</h1>
+        <p className='font-barcode-text'>Software Engineer</p>
       </div>      
+    </div>
+  )
+}
+
+type PromptProps = {
+  text: string,
+}
+const Prompt: React.FC<PromptProps> = ({ text }) => {
+  const [currentText, setCurrentText] = useState('')
+  const [cursorVisible, setCursorVisible] = useState(false)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCursorVisible(cursorVisible => !cursorVisible)
+    }, 500)
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <div className="flex flex-row text-surface mt-10 gap-2">
+      <div className='font-bold'>{'>'}</div>
+      <div>
+        {text}{cursorVisible ? '_' : ''}
+      </div>
     </div>
   )
 }
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-3 py-4">
+    <div className="container mx-auto px-5 py-4">
       <div className='flex flex-col'>
         <Header />
+        <Prompt text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil' />
       </div>
     </div>
   )
