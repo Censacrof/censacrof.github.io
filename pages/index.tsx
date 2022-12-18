f8bbeb8521465734381e62374cde43841c804959
Francesco Galisi <galisifrancesco@gmail.com>
2022-12-18 18:14:50 +0100

    Added colors to theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline text-asd">
       Hello world!
     </h1>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'background': '#101214',
+      'primary': '#101214',
+      'secondary': '#404040',
+      'surface': '#f3f3e8',
+    }
   },
   plugins: [],
 }
