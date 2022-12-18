28a0b9ebc9c492a58767d281ad37068fda865307
Francesco Galisi <galisifrancesco@gmail.com>
2022-12-18 21:18:20 +0100

    Created Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,26 @@ import styles from '../styles/Home.module.css'
 
 const monda = Monda({weight: '400'})
 
+const Header = () => {
+  return (
+    <div className="flex flex-col flex-wrap gap-4">
+      <div className='flex-grow flex flex-row justify-center items-center'>
+        <div className="bg-primary rounded-full h-[70px] w-[70px]"></div>
+      </div>
+      <div className="flex flex-col justify-center gap-2 items-center">
+        <h1 className='text-3xl text-surface'>Francesco Galisi</h1>
+        <p className='text-3xl font-barcode-text text-surface'>Software Engineer</p>
+      </div>      
+    </div>
+  )
+}
+
 export default function Home() {
   return (
-    <>
-      <h1 className='text-3xl font-bold underline text-surface'>
-        Hello world!
-      </h1>
-      <p className='font-barcode-text text-surface'>
-        Nice to meet you!
-      </p>
-    </>
+    <div className="container mx-auto px-3 py-4">
+      <div className='flex flex-col'>
+        <Header />
+      </div>
+    </div>
   )
 }
