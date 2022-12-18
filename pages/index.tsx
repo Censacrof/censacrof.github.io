d24e01460d97a801ba01275513e84de6e2d0e4bd
Francesco Galisi <galisifrancesco@gmail.com>
2022-12-18 21:36:52 +0100

    Removed useless import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Monda } from '@next/font/google'
-import styles from '../styles/Home.module.css'
 
 const monda = Monda({weight: '400'})
 
