c36a5f70a0f14515bd89225dac45bae47567fb35
Francesco Galisi <galisifrancesco@gmail.com>
2022-12-19 00:00:22 +0100

    Added bounce class to ScrollButton

--- components/ScrollButton.tsx
+++ components/ScrollButton.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const ScrollButton = () => {
   return (
-    <div className="fixed mx-auto inset-x-0 flex flex-row bottom-8 text-surface text-5xl justify-center items-center w-20 h-20">
+    <div className="fixed mx-auto inset-x-0 flex flex-row bottom-8 text-surface text-5xl justify-center items-center w-20 h-20 animate-bounce">
       <FontAwesomeIcon icon={ faAngleDown } />
     </div>
   )
