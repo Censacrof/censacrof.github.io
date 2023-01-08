300bfc8a0a077dc8fb255e6bfe7286648ec7d02d
Francesco Galisi <galisifrancesco@gmail.com>
2023-01-08 16:09:26 +0100

    Added profile image loaded directly from GitHub

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image"
+
 const Header = () => {
   return (
     <div className="flex flex-col flex-wrap gap-4">
       <div className='flex-grow flex flex-row justify-center items-center'>
-        <div className="bg-primary rounded-full h-[70px] w-[70px]"></div>
+        <div className="bg-primary relative rounded-full h-[70px] w-[70px] overflow-hidden">
+          <Image
+            src='https://github.com/Censacrof.png'
+            alt='Profile picture'
+            fill
+            className='w-full'
+          />
+        </div>
       </div>
       <div className="flex flex-col justify-center gap-2 items-center text-surface text-3xl ">
         <h1 className='text-3xl'>Francesco Galisi</h1>
